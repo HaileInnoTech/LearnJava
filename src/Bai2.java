becf932ea2be3b9a2e4d98b7eb14630441d7bd88
HellCursed2u <hai28022002@gmail.com>
2022-09-29 13:54:16 +0700

    Bài 2

--- src/Bai2.java
+++ src/Bai2.java
@@ -1,6 +1,6 @@
 public class Bai2 {
     public static void main(String[] args) {
-        System.out.println("Bai 2");
+        System.out.println("Bài 2");
         System.out.println(Byte.MAX_VALUE);
         System.out.println(Double.MIN_VALUE);
         System.out.println(Long.SIZE);
