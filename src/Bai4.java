0303421ded91f7fb2a041ad0dcb6e06344c47f3a
HellCursed2u <hai28022002@gmail.com>
2022-09-29 18:21:43 +0700

    Bài 4

---
+++ src/Bai4.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        double a = 100;
+        double b = -3;
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("a && b ? " + ((x && y)));
+        System.out.println("a || b ? " + ((x || y)));
+        System.out.println("!x = " + (!x));
+//      So sánh
+//      System.out.println("a == b ? "+ ((a==b)));
+//        System.out.println("a == b ? "+ ((a==b)));
+
+
+//        a = a*b;
+//        System.out.println("a:" + a);
+//        a*=b;
+//        System.out.println("a:" + a);
+
+//        System.out.println("a : " + a);
+//        System.out.println("b : " + b);
+//        System.out.println("sum : " + (a%b));
+    }
+}
