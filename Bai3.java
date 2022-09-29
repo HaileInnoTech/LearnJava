7f398105b8a65eace5dda6ace8d96baedbef9588
HellCursed2u <hai28022002@gmail.com>
2022-09-29 16:36:52 +0700

    Bài 3

--- Bai3.java
+++
@@ -1,9 +0,0 @@
-public class Bai3 {
-    public static void main(String[] args) {
-        int age;
-        float avgGrade;
-        long balance;
-        String  fullname;
-        String address;
-    }
-}
---
+++ src/Bai3.java
@@ -0,0 +1,11 @@
+public class Bai3 {
+    public static void main(String[] args) {
+        int age = 0;
+        float avgGrade = 0.0f;
+        long balance = 0;
+        String fullname;
+        String address;
+        System.out.println("Hello World");
+    }
+}
+
