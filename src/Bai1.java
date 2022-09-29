84895cebbc224d11b9cb799401ef34e48ee5640f
HellCursed2u <hai28022002@gmail.com>
2022-09-29 13:47:05 +0700

    Bai 2

--- src/Bai1.java
+++ src/Bai1.java
@@ -2,5 +2,13 @@ public class Bai1 {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("hello world");
+        System.out.println("My name is Hai Le");
+        // Ctrl + / trên dòng dể comment hoặc bỏ comment
+        // Alt + Ctrl + L để format code\
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Long.SIZE);
+        System.out.println(Float.SIZE);
+
     }
 }
---
+++ src/Bai2.java
@@ -0,0 +1,8 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Long.SIZE);
+        System.out.println(Float.SIZE);
+    }
+}
