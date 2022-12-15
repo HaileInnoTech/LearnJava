e718c42135654c0c6da0096b7d40018576c07add
HellCursed2u <hai28022002@gmail.com>
2022-12-15 22:16:07 +0700

    Bai 13

--- src/Bai13.java
+++ src/Bai13.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class Bai13 {
     public static void main(String[] args) {   //vd1 dem so luong chu cai nhap tu ban phim
         var input = new Scanner(System.in);
@@ -17,18 +19,52 @@ public class Bai13 {
 //
 
         //vd 2 tinh tong 1 >> 1/n, n nhaopp tu ban phim
-        System.out.println("Nhap so nguyen duong n");
-        var n = input.nextInt();
-        if (n > 0) {
-            var sum = 0.0;
-            for (int i = 1; i < n; i++) {
-                sum += 1.0 / i;
+//        System.out.println("Nhap so nguyen duong n");
+//        var n = input.nextInt();
+//        if (n > 0) {
+//            var sum = 0;
+//            for (int i = 1; i <= n; i++) {
+//                sum += i;
+//
+//            }
+//            System.out.println("S = "+sum);
+//        } else {
+//            System.out.println("Nhap n la so nguyen duong ");
+//        }
 
+
+//        System.out.println("Nhap vao chuoi ki tu");
+//        var str = input.nextLine();
+//        str = str.trim(); // loại bỏ các dấu cách thừa
+//        // thay thế các chỗ có 1 hoặc nhiều khoảng trắng bằng 1 dấu cách
+//        str = str.replace("\\s+", "");
+//        System.out.println("Chuỗi sau chuẩn hóa: " + str);
+//        for (int i = 0; i < str.length(); i++) {
+//            if (i == 0) { // in ra chữ cái đầu từ đầu tiên
+//                System.out.println(str.charAt(i));
+//            }
+//            if (str.charAt(i) == ' ') { // in ra chữ cái đầu từ tiếp theo
+//                System.out.println(str.charAt(i + 1));
+        // Tim so nguyen to trong doan tu a < b
+        System.out.println("Nhap so a");
+        var a = input.nextInt();
+        System.out.println("Nap so b");
+        var b = input.nextInt();
+
+        for (int i = a; i <= b; i++) {
+            boolean isPrime = true;
+            if(i < 2){ isPrime = false;}
+            else {
+                for (int j = 2; j < Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                    }
+                }
             }
-            System.out.println("S = "+sum);
-        } else {
-            System.out.println("Nhap65 n la so nguyen duong ");
+            System.out.print(isPrime ? i +", ":"");
         }
 
     }
 }
+
+
